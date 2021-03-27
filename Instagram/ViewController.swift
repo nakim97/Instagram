77f22ed60cfc2453b18a0fe2f51432c4c8a0cf3e
Na Kim <nak001@ucsd.edu>
2021-03-26 23:06:43 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Stella Kim on 3/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
