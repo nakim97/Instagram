add795f49ab7e7f8a1eeab81580bd7b1e127bdd8
Na Kim <nak001@ucsd.edu>
2021-03-27 18:19:52 -0400

    Adding "Sign Up and Log in" Features

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "YUeFfxrr0LmpiFVjmVW2XOBYI2ejJYHQTSqiEcmJ" // <- UPDATE
+                    $0.clientKey = "CJiTLvc25IPgVXOi6ZXNNdLqYCLft939gvpXHc13" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
